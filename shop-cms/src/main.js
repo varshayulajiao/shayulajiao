41121dd373ea39ba28868f7a0ebca2b1e96dc15d
shayulajiao <var666@qq.com>
2023-07-06 11:15:48 +0800

    创建脚手架 axios封装

---
+++ shop-cms/src/main.js
@@ -0,0 +1,26 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+
+
+import App from './App.vue'
+import router from './router'
+
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
+app.use(ElementPlus)
+app.use(router)
+
+app.mount('#app')
---
+++ shop-cms/src/utils/http.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import { useUsersStore } from '../stores/users';
+//通用功能
+const instance = axios.create({
+  //localhost:8080/api/ 
+  baseURL: '/api',
+  //请求超时
+  timeout: 5000
+});
+
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  const useStore = useUsersStore()
+ config.headers.Authorization= useStore.token
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
+
+
+// 统一请求接口的写法
+
+const http = {
+  // 查询数据
+  get(url, data, config) {
+    return instance.get(url, {...config, params: data})
+  },
+  // 添加数据
+  post(url, data, config) {
+    return instance.post(url, data, config)
+  },
+  // 整体更新
+  put(url, data, config) {
+    return instance.put(url, data, config)
+  },
+  // 局部更新
+  patch(url, data, config) {
+    return instance.patch(url, data, config)
+  },
+  // 删除数据
+  delete(url, data, config) {
+    return instance.delete(url, {...config, data})
+  }
+}
+
+
+export default http 
