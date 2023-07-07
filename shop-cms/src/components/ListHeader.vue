2fc57b9a5aced1c0231ddfdfe50c95e9fd3170dc
shayulajiao <var666@qq.com>
2023-07-07 11:52:22 +0800

    分类管理页面结构功能

---
+++ shop-cms/src/components/ListHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex-box">
+        <el-button type="primary" size="small">新增</el-button>
+        <el-tooltip effect="dark" content="刷新数据" placement="top">
+            <el-button class="icon-refresh">
+                <el-icon :size="20" >
+                    <Refresh />
+                </el-icon>
+            </el-button>
+        </el-tooltip>
+    </div>
+</template>
+
+<script setup>
+</script>
+<style scoped>
+.flex-box{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.icon-refresh{
+    border: 0;
+}
+</style>
