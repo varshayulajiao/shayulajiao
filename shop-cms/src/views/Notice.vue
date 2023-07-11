1a5e3e9024ee490219d5e2d3d24596871fa82456
hongzhiqi <3322984948@qq.com>
2023-07-11 20:58:28 +0800

    上传公告管理

--- shop-cms/src/views/Notice.vue
+++ shop-cms/src/views/Notice.vue
@@ -8,11 +8,11 @@
         <el-drawer v-model="drawer" title="新增" size="50%">
             <div>
                 <span style="color: red;">*</span>公告标题
-                <el-input v-model="input" placeholder="公告标题" />
+                <el-input v-model="input" placeholder="公告标题"/>
                 <span style="color: red;">*</span>公告内容
                 <el-input v-model="input1" placeholder="公告内容" />
-                <el-button type="primary" style="margin-top: 15px;">提交</el-button>
-                <el-button type="primary" style="margin-top: 15px;">取消</el-button>
+                <el-button type="primary" style="margin-top: 15px;" @click="onAddItem">提交</el-button>
+                <el-button type="primary" style="margin-top: 15px;" @click="drawer = false">取消</el-button>
             </div>
         </el-drawer>
 
@@ -22,24 +22,25 @@
 
         <el-table :data="tableData">
             <el-table-column prop="date" label="公告标题" width="300" />
-            <el-table-column prop="name" label="发布时间"  align="center"/>
+            <el-table-column prop="name" label="发布时间" align="center" />
             <el-table-column label="操作" align="right">
                 <template #default="scope">
-                    <el-button plain type="primary" style="margin-left: 16px" @click="handleModifyDrawer(scope.$index,scope.row)">修改</el-button>
-                    <el-button @click="handleCarouselDrawer(scope.$index,scope.row)" >删除</el-button>
+                    <el-button plain type="primary" style="margin-left: 16px"
+                        @click="handleModifyDrawer(scope.$index, scope.row)">修改</el-button>
+                    <el-button @click="handleCarouselDrawer(scope.$index, scope.row)">删除</el-button>
                 </template>
             </el-table-column>
         </el-table>
 
-        <el-drawer v-for="item in tableData" v-model="item.drawer1" title="修改" :with-header="false">
+        <el-drawer v-for="item,index in tableData" v-model="item.drawer1" title="修改" :with-header="false">
             修改
             <div>
                 <span style="color: red;">*</span>公告标题
-                <el-input v-model="input" />
+                <el-input v-model="input2" />
                 <span style="color: red;">*</span>公告内容
-                <el-input v-model="input1" />
-                <el-button type="primary" style="margin-top: 15px;">提交</el-button>
-                <el-button type="primary" style="margin-top: 15px;">取消</el-button>
+                <el-input v-model="input3" />
+                <el-button type="primary" style="margin-top: 15px;" @click="ontijiao(index)">提交</el-button>
+                <el-button type="primary" style="margin-top: 15px;" @click="item.drawer1 = false">取消</el-button>
             </div>
         </el-drawer>
 
@@ -51,10 +52,28 @@
 import { ref } from 'vue'
 import { ElMessageBox } from 'element-plus'
 import { ElMessage } from 'element-plus'
+import dayjs from 'dayjs'
+
 const input = ref('')
 const input1 = ref('')
+const input2 = ref('')
+const input3 = ref('')
 const drawer = ref(false)
 const innerDrawer = ref(false)
+const now = new Date()
+
+const onAddItem = () => {
+    now.setDate(now.getDate() + 1)
+    tableData.value.push({
+    date: input.value,
+    name: dayjs(now).format('YYYY-MM-DD hh:mm:ss'),
+    address: '',
+    drawer1: false
+})
+}
+const ontijiao = (index) => {
+    tableData.value[index].date = input2.value
+}
 
 const handleModifyDrawer = (index, row) => {
     tableData.value[index].drawer1 = !tableData.value[index].drawer1
@@ -67,13 +86,14 @@ const handleCarouselDrawer = (index, row) => {
             confirmButtonText: '是',
             cancelButtonText: '否',
             type: 'warning',
-        }
+        },
     )
         .then(() => {
             ElMessage({
                 type: 'success',
                 message: '删除成功',
             })
+            tableData.value.splice(index, 1)
         })
         .catch(() => {
             ElMessage({
@@ -173,5 +193,4 @@ const tableData = ref([
 .a4 {
     justify-content: center;
 }
-
 </style> 
